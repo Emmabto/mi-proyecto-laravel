778eeb8251ebbf72fcd940940e8a222f63af0ec1
Martín Emmanuel Bastida Toledo <bastidaemmanuel12@gmail.com>
2024-09-07 12:28:47 -0600

    Primer commit

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacto</title>
+</head>
+<body>
+    <h1>Formulario de contacto</h1>
+
+    <form action="/guardar-formulario" method="POST">
+        @csrf
+        <label for="nombre">Nombre:</label><br>
+        <input type="text" name="nombre"><br>
+
+        <label for="correo">Correo:</label><br>
+        <input type="email" name="correo"><br>
+
+        <label for="mensaje">Mensaje:</label><br>
+        <textarea name="mensaje" cols="30" rows="4"></textarea><br>
+
+        <input type="submit" value="Enviar">
+
+    </form>
+    
+</body>
+</html>
